89a31843fc5190f816b7e3592c49725fe6f01b6e
ShellyXueHan <shelly@freshworks.io>
2018-08-02 10:34:26 -0700

    add test of api

--- __tests__/api.spec.js
+++ __tests__/api.spec.js
@@ -21,8 +21,7 @@
 import { default as request } from 'supertest'; // eslint-disable-line
 import app from '../src';
 
-// jest.mock('request-promise-native');
-// jest.mock('minio');
+jest.mock('../src/libs/job');
 
 describe('Test monitoring routes', () => {
   test('The readiness probe should respond with 200 ', async () => {
@@ -31,30 +30,62 @@ describe('Test monitoring routes', () => {
   });
 });
 
-// describe('Test job routes', () => {
-//   test('Test a job must be present', async () => {
-//     const response = await request(app)
-//       .post('/v1/job');
-//     expect(response.statusCode).toBe(404); // Missing specific route path
-//   });
+describe('Test job routes', () => {
+  test('Test a job type must be present', async () => {
+    const response = await request(app)
+      .post('/v1/job');
+    expect(response.statusCode).toBe(404); // Missing specific route path
+  });
 
-//   test('Test deployment job must have a job in request body', async () => {
-//     const response = await request(app)
-//       .post('/v1/job/deploy')
-//       .set('content-type', 'application/json');
-//     expect(response.statusCode).toBe(400); // Bad request
-//   });
+  test('Test signing job must have a job in request body', async () => {
+    const response = await request(app)
+      .post('/v1/job/sign')
+      .set('content-type', 'application/json');
+    expect(response.statusCode).toBe(400); // Bad request
+  });
 
-//   test('Test deployment job must have specified both platforms', async () => {
-//     const response = await request(app)
-//       .post('/v1/job/deploy')
-//       .set('content-type', 'application/json')
-//       .send({
-//         job: {
-//           id: 1,
-//           platform: 'android',
-//         },
-//       });
-//     expect(response.statusCode).toBe(400); // Bad request
-//   });
-// });
+  test('Test signing job accepted', async () => {
+    const response = await request(app)
+      .post('/v1/job/sign')
+      .set('content-type', 'application/json')
+      .send({
+        id: 1,
+        name: 'hihi',
+      });
+    expect(response.statusCode).toBe(200); // OK
+  });
+
+  test('Test deployment job must have a job in request body', async () => {
+    const response = await request(app)
+      .post('/v1/job/deploy')
+      .set('content-type', 'application/json');
+    expect(response.statusCode).toBe(400); // Bad request
+  });
+
+  test('Test deployment job must have specified both platforms', async () => {
+    const response = await request(app)
+      .post('/v1/job/deploy')
+      .set('content-type', 'application/json')
+      .send({
+        job: {
+          id: 1,
+          platform: 'android',
+        },
+      });
+    expect(response.statusCode).toBe(400); // Bad request
+  });
+
+  test('Test deployment job accepted', async () => {
+    const response = await request(app)
+      .post('/v1/job/deploy')
+      .set('content-type', 'application/json')
+      .send({
+        job: {
+          id: 1,
+          platform: 'android',
+          deployPlatform: 'public',
+        },
+      });
+    expect(response.statusCode).toBe(200); // OK
+  });
+});
